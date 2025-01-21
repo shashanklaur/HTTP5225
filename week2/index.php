2dcb9720e1d8622e18bcdbc1b6340d15656a0290
Shashank Laur <shashanklaur@gmail.com>
2025-01-21 17:18:09 -0500

    fixed folder issue again

---
+++ week2/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+  <script>
+    document.write('Hello World in Javascript')
+    </script>
+  <?php
+  echo 'Welcome to Shashank\'s World';
+
+  ?>  
+</body>
+</html>
